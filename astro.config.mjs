43bb5fc1d988d2573247e32635c64eb95937ab7c
ayush <ayushviiithegreat@gmail.com>
2025-06-14 22:12:55 +0530

    push3

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 const owner = "astrojs";
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ayush-08-beep.github.io/real-ministry-of-ayush/'
+  site: 'https://ayush-08-beep.github.io/real-ministry-of-ayush/',
   integrations: [tailwind()],
 });
